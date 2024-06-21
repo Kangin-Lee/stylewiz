226477f28d15d4e876842aaf4ec305fedaf2bf52
Kangin Lee <cjsworkddls1@naver.com>
2024-06-21 20:07:59 +0900

    파일 index.tsx로 바꾸고 다크모드 버튼 생성

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import HomeSection1 from "./Section/HomeSection1";
-import HomeSection2 from "./Section/HomeSection2";
-import HomeSection3 from "./Section/HomeSection3";
+import HomeSection1 from "./HomeSection1";
+import HomeSection2 from "./HomeSection2";
+import HomeSection3 from "./HomeSection3";
 
 const Home: React.FC = () => {
   return (
